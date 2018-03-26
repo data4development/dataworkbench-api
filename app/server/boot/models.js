86e943b2ec14844014976c9d36be0506fb6857f0
Rolf Kleef <rolf@drostan.org>
2018-03-26 13:48:45 +0200

    Updates models
    - Adds uuid id for instances
    - Adds model relations and nested queries via the API
    - Adds descriptions to many properties
    - Adds extra properties for datasets

---
+++ app/server/boot/models.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function(app, cb) {
+  /*
+   * The `app` object provides access to a variety of LoopBack resources such as
+   * models (e.g. `app.models.YourModelName`) or data sources (e.g.
+   * `app.datasources.YourDataSource`). See
+   * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
+   * for more info.
+   */
+
+  app.models['iati-publisher'].nestRemoting('workspaces')
+
+  process.nextTick(cb); // Remove if you pass `cb` to an async function yourself
+};
