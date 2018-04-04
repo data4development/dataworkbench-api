da4594694328e7e14f6e0256abb8b2c6d2d68aa7
Bas Wanders <bas@wanderssoftware.com>
2018-04-04 13:48:27 +0200

    On file upload send pub/sub message

--- app/common/models/iati-testdataset.js
+++ app/common/models/iati-testdataset.js
@@ -1,5 +1,6 @@
 var CONTAINERS_URL = '/api/iati-testfile/';
 var CONTAINER_NAME = 'dataworkbench-test';
+var PUBSUB_TOPIC_NEW_FILE = 'newIatiFile';
 
 module.exports = function(File) {
 
@@ -23,6 +24,11 @@ module.exports = function(File) {
                         cb(err);
                     } else {
                         cb(null, obj);
+
+                        // Send message with pub/sub to inform the upload of the new file
+                        var pubSubMessage = {};
+                        pubSubMessage.tempWorkspaceId = options.ws;
+                        publishMessage(PUBSUB_TOPIC_NEW_FILE, pubSubMessage);
                     }
                 });
             }
@@ -43,5 +49,32 @@ module.exports = function(File) {
             http: {verb: 'post'}
         }
     );
+
+    // Send a message to Google Pub/sub to inform the upload of a new File
+    function publishMessage(topicName, data) {
+      // [START pubsub_publish_message]
+      // Imports the Google Cloud client library
+      const PubSub = require(`@google-cloud/pubsub`);
+
+      // Creates a client
+      // const pubsub = new PubSub();
+      const pubsub = new PubSub({keyFilename: 'local/DataWorkbench-serviceaccount.json'});
+
+      // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+      const dataBuffer = Buffer.from(JSON.stringify(data));
+
+      pubsub
+        .topic(topicName)
+        .publisher()
+        .publish(dataBuffer)
+        .then(messageId => {
+          console.log(`Message ${messageId} published.`);
+        })
+        .catch(err => {
+          console.error('ERROR:', err);
+        });
+      // [END pubsub_publish_message]
+    }
+
 
 };
