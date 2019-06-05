27f053e4655f32ef4cc4c93803f2c4aea0b8b5aa
Rolf Kleef <rolf@data4development.nl>
2019-06-05 12:21:08 +0200

    Remove pubsub from file upload
    Not using pubsub right now

--- common/models/iati-testdataset.js
+++ common/models/iati-testdataset.js
@@ -2,7 +2,6 @@
 
 var CONTAINERS_URL = '/api/iati-testfiles/';
 var CONTAINER_NAME = 'dataworkbench-test';
-var PUBSUB_TOPIC_NEW_FILE = 'newIatiFile';
 
 module.exports = function(File) {
   File.upload = function(ctx, options, cb) {
@@ -26,11 +25,6 @@ module.exports = function(File) {
             cb(err);
           } else {
             cb(null, obj);
-
-            // Send message with pub/sub to inform the upload of the new file
-            var pubSubMessage = {};
-            pubSubMessage.tempWorkspaceId = options.ws;
-            publishMessage(PUBSUB_TOPIC_NEW_FILE, pubSubMessage);
           }
         });
       }
@@ -52,30 +46,4 @@ module.exports = function(File) {
     }
   );
 
-  // Send a message to Google Pub/sub to inform the upload of a new File
-  function publishMessage(topicName, data) {
-    // [START pubsub_publish_message]
-    // Imports the Google Cloud client library
-    const PubSub = require('@google-cloud/pubsub');
-
-    // Creates a client
-    // const pubsub = new PubSub();
-    const pubsub = new PubSub({
-    	keyFilename: 'local/DataWorkbench-serviceaccount.json'});
-
-    // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
-    const dataBuffer = Buffer.from(JSON.stringify(data));
-
-    pubsub
-      .topic(topicName)
-      .publisher()
-      .publish(dataBuffer)
-      .then(messageId => {
-        console.log(`Message ${messageId} published.`);
-      })
-      .catch(err => {
-        console.error('ERROR:', err);
-      });
-    // [END pubsub_publish_message]
-  }
 };
