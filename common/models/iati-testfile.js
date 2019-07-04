bfd4cdf5e96f8c732550172ace15089920333c0e
Dmytro Panontko <dmytro.panontko@keenethics.com>
2019-07-04 16:10:20 +0300

    Hide backet name for downloading.

--- common/models/iati-testfile.js
+++ common/models/iati-testfile.js
@@ -1,5 +1,18 @@
 'use strict';
 
-module.exports = function(Iatitestfile) {
+var CANTAINER_NAME = 'dataworkbench-test-staging-d4d-dataworkbench';
 
+module.exports = function(Iatitestfile) {
+  Iatitestfile.file = function(req, res, filename, cb) {
+    Iatitestfile.download(CANTAINER_NAME, filename, req, res, cb);
+  };
+
+  Iatitestfile.remoteMethod('file', {
+    accepts: [
+      {arg: 'req', type: 'object', 'http': {source: 'req'}},
+      {arg: 'res', type: 'object', 'http': {source: 'res'}},
+      {arg: 'filename', type: 'string', required: true},
+    ],
+    http: {verb: 'get', path: '/file/:filename'},
+  });
 };
