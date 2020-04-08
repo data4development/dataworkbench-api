95b1b46204e29c06879183d00e8bf7a4fa5254f9
Dmytro Panontko <dmytro.panontko@keenethics.com>
2020-04-08 23:10:47 +0300

    refactoring: upgrade eslint and add precommit hook

--- test/1-api-ready.test.js
+++ test/1-api-ready.test.js
@@ -4,43 +4,41 @@ console.log(
   `Type of API "${process.env.API_TYPE === 'public' ? 'public' : 'private'}"`
 );
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var api = require('../server/server');
-var version = require('../server/config.local');
-
-var should = chai.should();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const api = require('../server/server');
+const version = require('../server/config.local');
 
 chai.use(chaiHttp);
 
-describe('When checking the API', function() {
-  it('should be in the test environment', function(done) {
+describe('When checking the API', () => {
+  it('should be in the test environment', (done) => {
     process.env.NODE_ENV.should.equal('test');
     done();
   });
 
-  it('should have the API available', function(done) {
+  it('should have the API available', (done) => {
     chai.request(api)
       .get('/')
-      .end(function(err, res) {
+      .end((err, res) => {
         res.should.have.status(200);
         done();
       });
   });
 
-  it('should have the API explorer available', function(done) {
+  it('should have the API explorer available', (done) => {
     chai.request(api)
       .get('/explorer')
-      .end(function(err, res) {
+      .end((err, res) => {
         res.should.have.status(200);
         done();
       });
   });
 
-  it('should have the ping endpoint available', function(done) {
+  it('should have the ping endpoint available', (done) => {
     chai.request(api)
-      .get(version.restApiRoot + '/ping')
-      .end(function(err, res) {
+      .get(`${version.restApiRoot}/ping`)
+      .end((err, res) => {
         res.should.have.status(200);
         done();
       });
